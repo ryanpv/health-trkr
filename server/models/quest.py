6bf61a8ffc45ecfefe182ddf40a567e28441719c
Ryan <ryanpvoong@gmail.com>
2025-04-17 17:50:51 -0400

    fixed response bug. incorrect class inheritance for quests

--- server/models/quest.py
+++ server/models/quest.py
@@ -5,12 +5,19 @@ from sqlalchemy import DateTime, text
 from sqlmodel import Column, Field, SQLModel
 
 
-class Quest(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+class QuestBase(SQLModel):
     title: str
     quest_type: str
+    user_id: int = Field(foreign_key="user.id")
+    quest_status: str = Field(default="incomplete")
+
+
+class Quest(QuestBase, table=True):
+    id: int = Field(default=None, primary_key=True)
     date: datetime = Field(
         sa_column=Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
     )
-    user_id: int = Field(foreign_key="user.id")
-    quest_status: str = Field(default="incomplete")
+
+
+class QuestCreate(QuestBase):
+    pass
--- server/routers/quest.py
+++ server/routers/quest.py
@@ -1,18 +1,14 @@
 import sqlalchemy
 from fastapi import APIRouter, Depends, HTTPException, Request
-from models.quest import Quest
+from models.quest import Quest, QuestBase, QuestCreate
 
 router = APIRouter()
 
 
-@router.post("/quest", response_model=Quest, status_code=201)
-async def create_quest(quest: Quest):
+@router.post("/quest", response_model=QuestBase, status_code=201)
+async def create_quest(quest: QuestCreate):
     try:
         print("Creating quest:", quest)
-        return {
-            "quest": quest,
-            "message": "Quest created successfully",
-            "quest_status": "incomplete",
-        }
+        return quest
     except Exception as e:
         print(f"Error creating quest: {e}")
