967ccc3114d38e18ba560c9410f0e05be9097eb6
Ryan <ryanpvoong@gmail.com>
2025-04-24 11:18:49 -0400

    missing letter from table

--- server/models/reward.py
+++ server/models/reward.py
@@ -5,10 +5,12 @@ from sqlmodel import Field, SQLModel
 
 
 class Reward(SQLModel, table=True):
-  id: int = Field(default=None, primary_key=True)
-  title: str
-  points_cost: int
-  user_id: int = Field(foreign_key="user.id")
-  status: str = Field(default="not_redeemed")
-  date: datetime = Field(sa_column=Column(DateTime, server_default=text("CURRENT_TIMESTAMP")))
+    id: int = Field(default=None, primary_key=True)
+    title: str
+    points_cost: int
+    user_id: int = Field(foreign_key="users.id")
+    status: str = Field(default="not_redeemed")
+    date: datetime = Field(
+        sa_column=Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    )
   
