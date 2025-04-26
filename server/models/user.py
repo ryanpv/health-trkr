0debe5fe333d84f6c4288d2b1a468ebde08801b6
Ryan <ryanpvoong@gmail.com>
2025-04-25 20:38:43 -0400

    separated user stats to new table for normalization

--- server/models/user.py
+++ server/models/user.py
@@ -6,14 +6,19 @@ from sqlmodel import Field, SQLModel
 
 
 class UserCreate(SQLModel):
-    id: int = Field(default=None, primary_key=True)
     firebase_uid: str
     displayName: str
     email: str
 
 
 class User(UserCreate, table=True):
+    id: int = Field(default=None, primary_key=True)
     __tablename__ = "users"
+
+
+class UserStats(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="users.id")
     total_points: int
     current_daily_streak: int
     last_daily_completed: datetime = Field(
