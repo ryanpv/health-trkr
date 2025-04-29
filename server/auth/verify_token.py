1d993958a181a11b9f121545b5071710a34c8722
Ryan <ryanpvoong@gmail.com>
2025-04-28 23:25:44 -0400

    added email verification check

--- server/auth/verify_token.py
+++ server/auth/verify_token.py
@@ -2,20 +2,26 @@ from fastapi import Depends, Header, HTTPException, status
 from firebase_admin import auth
 
 
-async def verify_token(authorization: str = Header(...)): 
-  if not authorization.startswith("Bearer "):
-    raise HTTPException(
-      status_code=status.HTTP_401_UNAUTHORIZED,
-      detail="Invalid token",
-    )
-  
-  token = authorization.split(" ")[1]
-  try: 
-    decoded_token = auth.verify_id_token(token)
-    return decoded_token["uid"]
-  except Exception as e:
-    print(f"Error verifying token: { e }")
-    raise HTTPException(
-      status_code=status.HTTP_401_UNAUTHORIZED,
-      detail="Invalid token"
-    )
+async def verify_token(authorization: str = Header(...)):
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+        )
+
+    token = authorization.split(" ")[1]
+    try:
+        decoded_token = auth.verify_id_token(token)
+
+        if not decoded_token.get("email_verified"):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Email has not been verified",
+            )
+
+        return decoded_token.get("email_verified")
+    except Exception as e:
+        print(f"Error verifying token: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unable to authorize user"
+        )
