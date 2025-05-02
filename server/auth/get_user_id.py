ec6d8547a545abfe3d1e2a9b0a006ef30678ce4c
Ryan <ryanpvoong@gmail.com>
2025-05-02 00:24:36 -0400

    refactoring function to get and cache user db id

--- server/auth/get_user_id.py
+++ server/auth/get_user_id.py
@@ -8,20 +8,27 @@ from models.user import User
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-db_uid = TTLCache(maxsize=100, ttl=3600)
-
-async def fetch_user_id(session: AsyncSession = Depends(get_session), firebase_uid: str = Depends(verify_token_basic)):
-  result = await session.execute(select(User).where(User.firebase_uid == firebase_uid))
-  user = result.scalar_one_or_none()
-  if not user:
-      raise HTTPException(status_code=404, detail="User not found")
-  return user.id
+uid_cache = TTLCache(maxsize=100, ttl=3600)
 
 
-def make_uid_key(func, session, firebase_uid):
-   return hashkey(firebase_uid)
+def uid_key(_, session, firebase_uid):
+    return hashkey(firebase_uid)
 
 
-@cachetools.cached(cache=db_uid, key=make_uid_key)
-async def get_cached_user_id(session, firebase_uid):
-   return await fetch_user_id(session, firebase_uid)
+async def fetch_user_id(
+    session: AsyncSession = Depends(get_session),
+    firebase_uid: str = Depends(verify_token_basic),
+):
+    result = await session.execute(
+        select(User).where(User.firebase_uid == firebase_uid)
+    )
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user.id
+
+
+@cachetools.cached(cache=uid_cache, key=uid_key)
+async def get_cached_uid(session, firebase_uid):
+    return await fetch_user_id(session=session, firebase_uid=firebase_uid)
