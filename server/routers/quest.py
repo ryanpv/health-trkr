023e53fe10bb0a7c1d74dc01ad3535168397d265
Ryan <ryanpvoong@gmail.com>
2025-05-02 20:24:10 -0400

    fetch handler for posting quest

--- server/routers/quest.py
+++ server/routers/quest.py
@@ -1,20 +1,38 @@
-import sqlalchemy
-from fastapi import APIRouter, Depends, HTTPException, Request
-from models.quest import QuestCreate, QuestCreateResponse
+from auth.verify_token_and_email import verify_token_and_email
+from database import get_session
+from fastapi import APIRouter, Depends, HTTPException
+from models.quest import Quest, QuestCreate, QuestCreateResponse
+from models.user import User
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from auth.get_user_id import get_cached_uid
 
 router = APIRouter()
 
 
+# CREATE QUEST
 @router.post("/quest", response_model=QuestCreateResponse, status_code=201)
-async def create_quest(quest: QuestCreate):
+async def create_quest(
+    quest_data: QuestCreate,
+    session: AsyncSession = Depends(get_session),
+    uid: str = Depends(verify_token_and_email),
+):
     try:
-        print("Creating quest:", quest)
-        quest_id = 1
-        return {
-            "id": quest_id,
-            "message": "Sucessfully created quest",
-        }
-        # return quest
+        user_id = await get_cached_uid(firebase_uid=uid)
+
+        new_quest = Quest(
+            title=quest_data.title,
+            quest_type=quest_data.quest_type,
+            quest_status=quest_data.quest_status,
+            user_id=user_id,
+        )
+
+        session.add(new_quest)
+        await session.commit()
+        await session.refresh(new_quest)
+
+        return {"id": new_quest.id, "message": "Sucessfully created quest"}
     except Exception as e:
         print(f"Error creating quest: {e}")
         raise HTTPException(
