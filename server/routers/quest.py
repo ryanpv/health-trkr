b532d24e574d6017c4d5dbfc0b70c5ea9b6d1f1c
Ryan <ryanpvoong@gmail.com>
2025-05-14 20:42:50 -0400

    added delete handler for quest

--- server/routers/quest.py
+++ server/routers/quest.py
@@ -2,7 +2,13 @@ from auth.verify_token_and_email import verify_token_and_email
 from auth.verify_token_basic import verify_token_basic
 from database import get_session
 from fastapi import APIRouter, Depends, HTTPException
-from models.quest import Quest, QuestCreate, QuestCreateResponse, QuestResponse
+from models.quest import (
+    Quest,
+    QuestCreate,
+    QuestCreateResponse,
+    QuestResponse,
+    QuestDelete,
+)
 from models.user import User
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -59,4 +65,37 @@ async def create_quest(
         print(f"Error creating quest: {e}")
         raise HTTPException(
             status_code=500, detail="Unable to create quest at this time"
+        )
+
+
+# DELETE QUEST
+@router.delete("/quests/{quest_id}", status_code=201)
+async def delete_quest(
+    quest_id: int,
+    session: AsyncSession = Depends(get_session),
+    uid: str = Depends(verify_token_and_email),
+):
+    try:
+        user_id = await get_cached_uid(firebase_uid=uid)
+
+        # Check if quest exists
+        result = await session.execute(
+            select(Quest).where(Quest.id == quest_id, Quest.user_id == user_id)
+        )
+
+        quest = result.scalar_one_or_none()
+
+        if not quest:
+            raise HTTPException(status_code=404, detail="Quest not found")
+
+        print(f"quest: {quest}")
+        await session.delete(quest)
+        await session.commit()
+
+        return {"message": "Quest deleted successfully."}
+
+    except Exception as e:
+        print(f"Error deleting quest: {e}")
+        raise HTTPException(
+            status_code=500, detail="Unable to delete quest at this time."
         )
