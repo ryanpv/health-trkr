194d6e3d924e7cc6d47968dc359767844c5c659e
Ryan <ryanpvoong@gmail.com>
2025-05-16 21:55:30 -0400

    adding additional type checking

--- server/auth/verify_token_basic.py
+++ server/auth/verify_token_basic.py
@@ -3,22 +3,22 @@ from firebase_admin import auth
 from firebase_admin.exceptions import FirebaseError
 
 
-async def verify_token_basic(authorization: str = Header(...)):
-  if not authorization.startswith("Bearer "):
-    raise HTTPException(
+async def verify_token_basic(authorization: str = Header(...)) -> str:
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(
       status_code=status.HTTP_401_UNAUTHORIZED,
       detail="Invalid token"
     )
-  
-  token = authorization.split(" ")[1]
-  try:
-    decoded_token = auth.verify_id_token(token)
 
-    return decoded_token.get("uid")
-  except FirebaseError as e:
-    print(f"Firebase error: { e }")
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid/expired token")
+    token = authorization.split(" ")[1]
+    try:
+        decoded_token = auth.verify_id_token(token)
 
-  except Exception as e:
-    print(f"Error verifying token: { e }")
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Failed to verify token.")
+        return decoded_token.get("uid")
+    except FirebaseError as e:
+        print(f"Firebase error: { e }")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid/expired token")
+
+    except Exception as e:
+        print(f"Error verifying token: { e }")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Failed to verify token.")
