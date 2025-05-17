af527f782954924f64fbd445169b9324ab83078d
Ryan <ryanpvoong@gmail.com>
2025-05-16 21:56:26 -0400

    added check for if user id does not return

--- server/auth/get_user_id.py
+++ server/auth/get_user_id.py
@@ -5,14 +5,15 @@ from models.user import User
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from async_lru import alru_cache
+from typing import cast
 
 
 async def fetch_user_id(
     session: AsyncSession = Depends(get_session),
     firebase_uid: str = Depends(verify_token_basic),
-):
+) -> int:
     result = await session.execute(
-        select(User).where(User.firebase_uid == firebase_uid)
+        select(User).where(User.firebase_uid == firebase_uid) # type: ignore
     )
     user = result.scalar_one_or_none()
 
@@ -22,6 +23,13 @@ async def fetch_user_id(
 
 
 @alru_cache(maxsize=100)
-async def get_cached_uid(firebase_uid: str):
+async def get_cached_uid(firebase_uid: str) -> int:
     async with async_session() as session:
-        return await fetch_user_id(session=session, firebase_uid=firebase_uid)
+        user_id = await fetch_user_id(session=session, firebase_uid=firebase_uid)
+
+        if not isinstance(user_id, int):
+            raise ValueError(
+                f"Expected userID as int, but got: {type(user_id)} with value {user_id}"
+            )
+
+        return cast(int, user_id)
