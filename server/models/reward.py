62a30089f484218c18931bdfa3b8b67a3a68d7fa
Ryan <ryanpvoong@gmail.com>
2025-05-26 20:24:14 -0400

    default value for reward points

--- server/models/reward.py
+++ server/models/reward.py
@@ -7,7 +7,7 @@ from sqlmodel import Field, SQLModel
 class RewardBase(SQLModel):
     title: str
     status: str = Field(default="not_redeemed")
-    points_cost: int
+    points_cost: int = Field(default=100)
 
 
 class Reward(RewardBase, table=True): # type: ignore
