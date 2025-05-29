efd8db49c95410c5c9c9cde7c64b04cd7acab689
Ryan <ryanpvoong@gmail.com>
2025-05-28 23:32:20 -0400

    added fetch to logout route to remove cache/session

--- client/app/utils/logout.ts
+++ client/app/utils/logout.ts
@@ -8,16 +8,28 @@ export const logout = async(
   setLoading: Dispatch<SetStateAction<boolean>>, 
   setModalVisible: Dispatch<SetStateAction<boolean>>, 
   setError: Dispatch<SetStateAction<string | null>>,
+  setCurrentUser: Dispatch<SetStateAction<string | null>>
 ) => {
   try {
     setLoading(true);
-    const userSignOut = await signOut(FIREBASE_AUTH);
+    const serverUrl = process.env.EXPO_PUBLIC_DEV_SERVER;
+    const credentials = FIREBASE_AUTH.currentUser;
+    const accessToken = await credentials?.getIdToken();
 
+    await fetch(`${ serverUrl }/logout`, {
+      method: "DELETE",
+      headers: {
+        "authorization": `Bearer ${ accessToken }`,
+      }
+    });
+    
+    const userSignOut = await signOut(FIREBASE_AUTH);
     await Promise.all([
       deleteCredentials("uid"),
       deleteCredentials("displayName"),
     ]);
 
+    setCurrentUser("InitialHomie");
     setModalVisible(false);
     console.log("User logged out: ", userSignOut);
   } catch (error) {
