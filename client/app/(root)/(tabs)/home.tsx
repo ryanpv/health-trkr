d83c1f80186618b5d22748100bb47d0eba8cd9de
Ryan <ryanpvoong@gmail.com>
2025-06-03 20:56:29 -0400

    updated props and prop data to be passed to quest modal

--- client/app/(root)/(tabs)/home.tsx
+++ client/app/(root)/(tabs)/home.tsx
@@ -1,17 +1,26 @@
 import { SafeAreaView } from "react-native-safe-area-context"
-import { Text, View, Image, TouchableOpacity } from "react-native"
+import { Text, View, Image, TouchableOpacity, ScrollView } from "react-native"
 import { useEffect, useState } from "react";
 import { icons } from '@/constants';
 
-import QuestButton from "@/app/components/questButton";
-import AddQuestModal from "@/app/(quests)/create";
+import QuestButton from "@/app/components/quests/questButton";
+import AddQuestModal from "@/app/components/quests/create";
 import { FIREBASE_AUTH } from "@/FirebaseConfig";
 import { fetchQuests } from "@/app/utils/api";
 import { getUserAccessToken } from "@/app/utils/getAccessToken";
 import { useStateContext } from "@/app/contexts/stateContext";
 
 import { Quest } from "@/app/types/quest.types";
-import QuestModal from "@/app/components/questModal";
+import QuestModal from "@/app/components/quests/questModal";
+
+
+type QuestData = {
+  title: string,
+  id: number,
+  questStatus: string,
+  questType: string,
+  date: string
+}
 
 
 const Home = () => {
@@ -19,12 +28,17 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   const { questList, setQuestList } = useStateContext();
-  const [questModal, setQuestModal] = useState<{ title: string, id: number }>({ title: "", id: 0 });
+  const [questModalData, setQuestModalData] = useState<QuestData>({
+    title: '',
+    id: 0,
+    questStatus: '',
+    questType: '',
+    date: ''
+  });
   
   const serverUrl = process.env.EXPO_PUBLIC_DEV_SERVER;
   const credentials = FIREBASE_AUTH.currentUser;
 
-    
   
   useEffect(() => {
     const getQuests = async () => {
@@ -47,7 +61,7 @@ const Home = () => {
   }, []);
 
   return (
-    <SafeAreaView className="bg-blue-400 min-h-screen flex items-center p-5">
+    <SafeAreaView className="bg-blue-400 h-screen flex items-center p-5">
       <View className="flex max-w-xl w-full p-5">
         <View className="my-5 flex flex-row">
           <View className="flex-1 gap-y-2">
@@ -77,29 +91,38 @@ const Home = () => {
             goals completed
           </Text>
         </View>
-
-        <View className="gap-y-5">
-          { !loading && questList.length > 0 ?
-            questList.map((quest) => (
-              <QuestButton 
-                title={ quest.title } 
-                questType={ quest.quest_type } 
-                questStatus={ quest.quest_status }
-                modalVisible={ modalVisible }
-                setModalVisible={ setModalVisible }
-                onPress={ () => {
-                  setQuestModal({ title: quest.title, id: quest.id });    
-                  setModalVisible(true); 
-                } 
-              }
-              />
-            ))
-            : null
-          }
-        </View>
+        
+        <ScrollView className="max-h-[45vh]">
+          <View className="gap-y-5">
+            { !loading && questList?.length > 0 ?
+              questList.map((quest) => (
+                <QuestButton 
+                  key={ quest.id }
+                  title={ quest.title } 
+                  questType={ quest.quest_type } 
+                  questStatus={ quest.quest_status }
+                  modalVisible={ modalVisible }
+                  setModalVisible={ setModalVisible }
+                  onPress={ () => {
+                    setQuestModalData({
+                      title: quest.title,
+                      id: quest.id,
+                      questStatus: quest.quest_status,
+                      questType: quest.quest_type,
+                      date: quest.date
+                    });    
+                    setModalVisible(true); 
+                  } 
+                }
+                />
+              ))
+              : null
+            }
+          </View>
+        </ScrollView>
 
         <View>
-          <QuestModal closeModal={ () => setModalVisible(false) } modalVisible={ modalVisible } questTitle={ questModal.title } questId={ questModal.id } />
+          <QuestModal closeModal={ () => setModalVisible(false) } modalVisible={ modalVisible } questData={ questModalData } />
         </View>
 
         <View>
