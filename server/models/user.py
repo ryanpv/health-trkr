cace0986fda5047768e97ba3a0c385e3685a0086
Ryan <ryanpvoong@gmail.com>
2025-06-04 20:37:30 -0400

    small change to separate user stats class

--- server/models/user.py
+++ server/models/user.py
@@ -11,20 +11,6 @@ class UserCreate(SQLModel):
     email: str
 
 
-class User(UserCreate, table=True):
+class User(UserCreate, table=True): # type: ignore
     id: int = Field(default=None, primary_key=True)
-    __tablename__ = "users" # type: ignore
-
-
-class UserStats(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="users.id")
-    total_points: int
-    current_daily_streak: int
-    last_daily_completed: datetime = Field(
-        sa_column=Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
-    )
-    current_weekly_streak: int
-    last_weekly_completed: datetime = Field(
-        sa_column=Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
-    )
+    __tablename__ = "users" 
