1ac3062e7f28b690237cee4a942d47bc11ec433c
Ryan <ryanpvoong@gmail.com>
2025-06-07 20:23:32 -0400

    util for fetching user rewards

--- client/app/utils/api.ts
+++ client/app/utils/api.ts
@@ -3,9 +3,7 @@ export const fetchQuests = async(accessToken: string | undefined) => {
     const response = await fetch(`${ process.env.EXPO_PUBLIC_DEV_SERVER }/quests`, {
       method: "GET",
       headers: {
-        "Authorization": `Bearer ${ accessToken }`,
-        "Content-type": "application/json"
-      }
+        "Authorization": `Bearer ${ accessToken }`      }
     });
 
     if (!response.ok) {
@@ -15,5 +13,24 @@ export const fetchQuests = async(accessToken: string | undefined) => {
     return response.json();
   } catch (error: unknown) {
     console.log("fetchQuests error: ", error)
+  }
+}
+
+export const fetchRewards = async(accessToken: string | undefined) => {
+  try {
+    const response = await fetch(`${ process.env.EXPO_PUBLIC_DEV_SERVER }/rewards`, {
+      method: "GET",
+      headers: {
+        "authorization": `Bearer ${ accessToken }`
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch user rewards.")
+    }
+
+    return response.json();
+  } catch (error: unknown) {
+    console.log("Unable to retrieve rewards: ", error)
   }
 }
