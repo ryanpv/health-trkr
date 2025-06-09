93aca7c64afb2c2e9600de2095406675394c020f
Ryan <ryanpvoong@gmail.com>
2025-06-09 19:46:27 -0400

    created route to get user rewards

--- server/routers/reward.py
+++ server/routers/reward.py
@@ -3,14 +3,14 @@ from auth.verify_token_and_email import verify_token_and_email
 from database import get_session
 from fastapi import APIRouter, Depends, HTTPException
 from models.reward import Reward, RewardCreate
-from sqlalchemy import select
+from sqlalchemy import asc, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
 
 # POST REWARD
-@router.post("/rewards", status_code=200)
+@router.post("/rewards", status_code=201)
 async def create_reward(
     reward_data: RewardCreate,
     session: AsyncSession = Depends(get_session),
@@ -38,5 +38,25 @@ async def create_reward(
 
 
 
-
 # GET REWARD
+@router.get("/rewards", status_code=200)
+async def get_rewards(
+    session: AsyncSession = Depends(get_session),
+    uid: str = Depends(verify_token_and_email)
+):
+  try:
+    user_id = await get_cached_uid(firebase_uid=uid)
+
+    result = await session.execute(
+      select(Reward).where(Reward.user_id == user_id).order_by(asc(Reward.date)) # type: ignore
+    )
+
+    rewards = result.scalars().all()
+
+    return rewards
+
+  except Exception as e:
+    print(f"Unable to retrieve user's rewards: {e}")
+    raise HTTPException(
+      status_code=500, detail="Unable to retrieve user rewards."
+    )
