490b524dcfd33615c7551149df72d7751385aa4f
Ryan <ryanpvoong@gmail.com>
2025-06-16 19:11:45 -0400

    separating types for DRY

---
+++ client/app/types/state.types.ts
@@ -0,0 +1,20 @@
+export type Quest = {
+  date: string,
+  id: number,
+  quest_status: string,
+  quest_type: string,
+  title: string
+};
+
+export type Reward = {
+  date?: string,
+  id: number,
+  title: string,
+  points_cost:  number,
+};
+
+export type User = {
+  email: string,
+  displayName: string,
+  totalPoints: number;
+};
