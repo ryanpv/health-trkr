404c84f04ab5a01b6b212dd77e414cbe82f05c4d
Ryan <ryanpvoong@gmail.com>
2025-06-21 10:58:36 -0400

    added toast message for insufficient points

--- client/app/components/rewards/rewardButton.tsx
+++ client/app/components/rewards/rewardButton.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
+import Toast from "react-native-toast-message";
 
 
 interface RewardButtonProps {
   title: string;
   onPress: () => void;
+  points: number;
 }
 
-const RewardButton: React.FC<RewardButtonProps> = ({ title, onPress }) => {
+const RewardButton: React.FC<RewardButtonProps> = ({ title, onPress, points }) => {
+  const showToast = () => {
+    Toast.show({
+      type: 'error',
+      text1: 'No reward points to claim.',
+      text2: 'Complete more quests to earn reward points! 👋'
+    });
+  }
 
   return (
     <TouchableOpacity
-      onPress={ onPress }
+      onPress={ points <= 0 ? showToast : onPress }
     >
       <View
         className="p-2 shadow-lg rounded-md bg-blue-300 flex flex-row justify-between"
