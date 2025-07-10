f23df26622e4ccbd705742b078cd2492de6aee29
Ryan <ryanpvoong@gmail.com>
2025-07-09 20:06:37 -0400

    updated model to avoid wrong default values

--- server/models/user_stats.py
+++ server/models/user_stats.py
@@ -3,20 +3,21 @@ from typing import Optional
 
 from sqlalchemy import Column, DateTime, text
 from sqlmodel import Field, SQLModel
+from traitlets import default
 
 
 class UserStats(SQLModel, table=True): # type: ignore
     __tablename__ = "user_stats" 
     id: int = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="users.id")
-    total_points: int
-    current_daily_streak: int
-    last_daily_completed: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
+    total_points: int = Field(default=0)
+    current_daily_streak: int = Field(default=0)
+    last_daily_completed: Optional[datetime] = Field(
+        sa_column=Column(DateTime(timezone=True), default=None)
     )
-    current_weekly_streak: int
-    last_weekly_completed: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
+    current_weekly_streak: int = Field(default=0)
+    last_weekly_completed: Optional[datetime] = Field(
+        sa_column=Column(DateTime(timezone=True), default=None)
     )
     daily_bonus_claimed_at: Optional[datetime] = Field(default=None)
 
